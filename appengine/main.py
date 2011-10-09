d261d7908d9024bee237cba7d1e71b48d4fe3a42
Damianos Mylonakis <danmylonakis@gmail.com>
2011-10-09 04:00:45 +0300

    fix webapp2_extras package

--- appengine/main.py
+++ appengine/main.py
@@ -12,14 +12,12 @@ sys.path[0:0] = [
 import webapp2
 from config import config
 from urls import routes
-import error_handlers
 
 # Is this the development server?
 debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 # Instantiate the application.
 app = webapp2.WSGIApplication(routes=routes, config=config, debug=debug)
-error_handlers.register(app)
 
 def main():
     app.run()
--- appengine/urls.py
+++ appengine/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from handlers.handlers import Root
 routes = [
-    ('/', 'handlers.handlers.Root')
+    ('/', Root)
 ]
